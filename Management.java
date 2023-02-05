bd535c3688db7f9bf2ae488851501119ea0bde07
Ömer Edip Aras <113944382+omerediparas@users.noreply.github.com>
2023-02-05 22:28:45 +0300

    Files with initial design has been uploaded

---
+++ Management.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Management {
+    /**
+     * This main method provides an appropriately designed string based or printed user interface 
+     * Asks users to type specific characters to perform operations via calling other methods
+     * (e.g. 1 to create array 2 to sum up odd indexes etc)
+     * Main is said to be obligatory. Therefore I removed display method on the other file.
+    */
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Methods.java
@@ -0,0 +1,77 @@
+/**
+ * CS102-03  Lab0_Q2   05/02/23
+ * Contributors: Ömer Edip Aras, İbrahim Çaycı, Mustafa Çağlar and Yiğit Özhan 
+ * This program is menu-driven console application that allows users to create and modify an array with a proper console design.
+ */
+public class Methods {
+    
+    /**
+     * This method creates an array whose numbers are randomly selected.
+     * @param arraySize size of the array that we are creating.
+     * @return randomly determined integer array.
+    */
+    public int createArray(int arraySize){
+       /* TODO */ 
+    }
+    
+    /**
+     * This method provides an appropriately designed string based or printed user interface 
+     * Asks users to type specific characters to perform operations via calling other methods
+     * (e.g. 1 to create array 2 to sum up odd indexes etc)
+     * Main is said to be obligatory. Therefore I removed display method on the other file.
+    */
+    public void display (){
+        /* TODO 
+
+        createArray*/ 
+    }
+
+    /**
+     * This method finds the gratest element in an array
+     * @param array 
+     * @return the greatest integer of an array
+     */
+    public int getMaxNumber(int [] array){
+         /* TODO */ 
+    }
+
+    /**
+     * This method finds the smallest element in an array
+     * @param array 
+     * @return the smallest integer of an array
+     */
+    public int getMinNumber(int [] array){
+         /* TODO */ 
+    }
+
+    /**
+     * This method finds the AVERAGE INTEGER and then creates a new array by substracting this average number from each element.
+     * example {1,2,3,4,5,6} ave = 3  newArray = {-2,-1,0,1,2,3}
+     * @param array
+     * @return the inverted / substracted array
+     */
+    public int invertedArray(int [] array){
+         /* TODO */ 
+    }
+
+    /**
+     * This method calculates the sum of the array elements with even indexes 
+     * @param array
+     * @return returns the elements of the array with even indexes.
+     */
+    public int sumOfEven(int [] array){
+
+    }
+
+    /**
+     * This method calculates the sum of the array elements with odd indexes 
+     * @param array
+     * @return returns the elements of the array with odd indexes.
+     */
+    public int sumOfOdd(int [] array){
+
+    }
+
+
+
+}
